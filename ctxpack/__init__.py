ecb495a36562d2a37c673092325ec5f716ca14e2
chu2bard <bxndnbdnd29@gmail.com>
2025-08-26 10:12:00 +0200

    fix schema parsing

---
+++ ctxpack/__init__.py
@@ -0,0 +1,4 @@
+from ctxpack.manager import ContextManager
+from ctxpack.compressor import Compressor
+
+__version__ = "0.1.0"
---
+++ ctxpack/utils.py
@@ -0,0 +1,21 @@
+"""Token counting utilities."""
+import tiktoken
+
+
+def count_tokens(text: str, model: str = "gpt-4") -> int:
+    try:
+        enc = tiktoken.encoding_for_model(model)
+    except KeyError:
+        enc = tiktoken.get_encoding("cl100k_base")
+    return len(enc.encode(text))
+
+
+def truncate_to_tokens(text: str, max_tokens: int, model: str = "gpt-4") -> str:
+    try:
+        enc = tiktoken.encoding_for_model(model)
+    except KeyError:
+        enc = tiktoken.get_encoding("cl100k_base")
+    tokens = enc.encode(text)
+    if len(tokens) <= max_tokens:
+        return text
+    return enc.decode(tokens[:max_tokens])
